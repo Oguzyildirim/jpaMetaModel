51b6f7d234b55c651c1c5d5a2955a1edfcc5cfcb
Oguzhan Yildirim <oguzhanyildirim@protonmail.com>
2019-03-13 19:00:05 +0300

    jhipster filter generated

--- README.md
+++ README.md
@@ -147,3 +147,4 @@ To configure CI for your project, run the ci-cd sub-generator (`jhipster ci-cd`)
 [Protractor]: https://angular.github.io/protractor/
 [Leaflet]: http://leafletjs.com/
 [DefinitelyTyped]: http://definitelytyped.org/
+# jpaMetaModel
--- src/main/webapp/app/shared/layout/header/menus/entities.tsx
+++ src/main/webapp/app/shared/layout/header/menus/entities.tsx
@@ -7,6 +7,27 @@ import { NavDropdown } from '../header-components';
 export const EntitiesMenu = props => (
   // tslint:disable-next-line:jsx-self-close
   <NavDropdown icon="th-list" name="Entities" id="entity-menu">
+    <DropdownItem tag={Link} to="/entity/product">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Product
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/entity/product-category">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Product Category
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/entity/customer">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Customer
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/entity/product-order">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Product Order
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/entity/order-item">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Order Item
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/entity/invoice">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Invoice
+    </DropdownItem>
+    <DropdownItem tag={Link} to="/entity/shipment">
+      <FontAwesomeIcon icon="asterisk" />&nbsp; Shipment
+    </DropdownItem>
     {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
   </NavDropdown>
 );
